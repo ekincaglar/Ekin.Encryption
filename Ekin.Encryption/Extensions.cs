de3c2d89723cd758bdda1ba37ef2ec8fe70caa68
Ekin Caglar <ekincaglar@outlook.com>
2019-12-02 13:56:08 +0000

    Migration from VS Online

---
+++ Ekin.Encryption/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Ekin.Encryption
+{
+    public static class Extensions
+    {
+        public static string Encrypt(this string str, string Key)
+        {
+            return AESThenHMAC.SimpleEncryptWithPassword(str, Key);
+        }
+
+        public static string Decrypt(this string str, string Key)
+        {
+            return AESThenHMAC.SimpleDecryptWithPassword(str, Key);
+        }
+    }
+}
